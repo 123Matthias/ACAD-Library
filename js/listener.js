20c01b48742823fdcef2f18403a0da3bee471161
Matthias Leiter <nobody@noname.nix>
2026-02-19 20:05:40 +0100

    toggler

---
+++ js/listener.js
@@ -0,0 +1,17 @@
+
+
+
+async function loadNavbarListener() {
+    const toggler = document.querySelector('.navbar-toggler');
+    const menu = document.getElementById('mainNavbar');
+    menu.addEventListener('shown.bs.collapse', () => {
+        toggler.querySelector('.fa-bars').classList.add('d-none');
+        toggler.querySelector('.fa-xmark').classList.remove('d-none');
+    });
+
+    menu.addEventListener('hidden.bs.collapse', () => {
+        toggler.querySelector('.fa-bars').classList.remove('d-none');
+        toggler.querySelector('.fa-xmark').classList.add('d-none');
+    });
+
+}
